550a72ada22e969441d981adc774b945c95aaaa3
Cian Skelly <cianskelly@hotmail.com>
2018-05-21 15:50:10 +0100

    Added navbar (again possibly!??) Added unit testing -  HomeControllerTest.cs

--- LibraryBookSystem.Tests/Controllers/HomeControllerTest.cs
+++ LibraryBookSystem.Tests/Controllers/HomeControllerTest.cs
@@ -12,43 +12,14 @@ namespace LibraryBookSystem.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
+
         [TestMethod]
-        public void Index()
+        public void AboutTest1()
         {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
+            var controller = new HomeController();
+            var result = controller.About();
             Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
     }
 }
